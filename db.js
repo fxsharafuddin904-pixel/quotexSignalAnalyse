4e850a91beeeb0179846e22b23f05069478e9cf7
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-12 23:50:46 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,21 @@
+const { Pool } = require("pg");
+require("dotenv").config();
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+// Test Database Connection
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error("❌ Database connection failed:", err.message);
+  } else {
+    console.log("✅ PostgreSQL Connected");
+    release();
+  }
+});
+
+module.exports = pool;
